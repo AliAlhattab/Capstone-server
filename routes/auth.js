3fdd9a0f2d1ce27327278f36e2a5cfe239196bf9
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-06-28 17:56:51 -0400

    when signing up username must be unique

--- routes/auth.js
+++ routes/auth.js
@@ -3,22 +3,37 @@ const router = express.Router();
 const knex = require('knex')(require('../knexfile.js').development);
 
 router.post('/signup', (req, res) => {
+    
+   
     const { first_name, last_name, username, password } = req.body;
     const newUser = {first_name, last_name, username, password};
-    knex.insert(newUser).into("usersinfo").then(() => {
-        knex('usersinfo')
-        .then((data) => {
-            res.status(200).json(data);
-        })
-        .catch((err) => {
-            res.status(400).json(`could not sign up ${err}`)
-        })
+    knex
+    .select('username')
+    .from('usersinfo')
+    .where('username', username)
+    .then((result) => {
+        if(!result.length) {
+            knex('usersinfo')
+            .insert(newUser)
+            .into('usersinfo')
+            .then(() => {
+                knex('usersinfo')
+                .then((data) => {
+                    res.status(200).json(data);
+                })
+                .catch((err) => {
+                    res.status(400).json(`could not sign up ${err}`)
+                })
+            })
+        } else {
+            res.status(400).send("Username already exists");
+        }
     })
 })
 
 router.post('/login', (req, res) => {
-  
     
+
 })
 
 
